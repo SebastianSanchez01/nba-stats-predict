fecb82cef5d1b84f9e616c87ba4dd20ee0200e55
evan <evanchenx7@gmail.com>
2024-10-30 02:40:11 -0400

    added navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
+/* eslint-disable react/react-in-jsx-scope */
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Noto_Sans } from "next/font/google"; 
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const noto = Noto_Sans({subsets: ['latin'], weight: ['500']}); 
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,11 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={noto.className}>{children}</body>
     </html>
   );
 }
